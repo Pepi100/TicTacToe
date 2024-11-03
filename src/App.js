a61db6e2bae62e4ce64121e3ac9d1590813db879
Pepi100 <radunedelcu02@gmail.com>
2024-11-03 15:25:49 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
+import {Board} from './Components/Board';
+import { ScoreBoard } from "./Components/ScoreBoard";
+
 function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlaying, setXPlaying] = useState(false);
+  const [scores, setScores] = useState({xScore: 0, oScore: 0});
+
+  const handeBoxClick = (boxIdx) => {
+
+    const updatedBoard = board.map((value, idx) => {
+      if(idx === boxIdx && value === null ){
+        setXPlaying(!xPlaying);
+        return xPlaying === true ? "X" : "O";
+      }else{
+        return value;
+      }
+    })
+
+    const win = checkWinner(updatedBoard);
+
+    if(win){
+      if(win === "O"){
+        setScores(prevScores => ({
+          ...prevScores,       // Keep the existing properties of scores
+          oScore: prevScores.oScore + 1  // Increment oScore by 1
+        }));
+        
+      }else{
+        setScores(prevScores => ({
+          ...prevScores,       // Keep the existing properties of scores
+          xScore: prevScores.xScore + 1  // Increment oScore by 1
+        })); 
+      }
+
+      //clear table
+
+    }
+
+    setBoard(updatedBoard);
+    
+
+  }
+
+  const checkWinner = () => {
+    // check diagonals
+    if((board[0] === board[4] && board[4] === board[8]) || (board[2] === board[4] && board[4] === board[6]))
+      return board[4];
+    
+    //check columns
+    for(let i = 0; i<=2; i++){
+      if(board[i] === board[i+3] && board[i+3] === board[i+6])
+        return board[i];
+    }
+
+    //check rows
+    for(let i = 0; i<=6; i+=3){
+      if(board[i] === board[i+1] && board[i+1] === board[i+2])
+        return board[i];
+    }
+
+      return null;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScoreBoard xPlaying = {xPlaying} scores = {scores}></ScoreBoard>
+      <Board board = {board} onClick={handeBoxClick}> </Board>
     </div>
   );
 }
